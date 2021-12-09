f14e66502135d42ed5bf48fb192f9359f8e41c1b
Zoeyzhu <67898140+Maihao-zhu@users.noreply.github.com>
2021-12-08 22:40:41 -0500

    Update Algorithm.py

--- Algorithm.py
+++ Algorithm.py
@@ -21,3 +21,14 @@ for a in range(0,1001):
 end_time=time.time()
 print("time:%d"%(end_time-start_time))
 print("finished")
+
+#import time
+#start_time=time.time()
+#   for a in range(0,1001):
+#       for b in range(0,1001):
+#           for c in range(0,1001):
+#           if a+b+c==1000 and a**2+b**2==c**2:
+#               print("a,b,c:%d,%d,%d"%(a,b,c))
+#end_time=time.time()
+#print("time:%d"%(end_time-start_time))
+#print("finished")
